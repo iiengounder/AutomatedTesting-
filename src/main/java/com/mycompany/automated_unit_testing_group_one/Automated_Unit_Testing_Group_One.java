7767af4baf6e42783a87afc9d884ddc811e1b45a
iiengounder <166714905+iiengounder@users.noreply.github.com>
2024-05-17 11:42:49 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/automated_unit_testing_group_one/Automated_Unit_Testing_Group_One.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.automated_unit_testing_group_one;
+
+/**
+ *
+ * @author Novashnee
+ */
+public class Automated_Unit_Testing_Group_One {
+
+    public static void main(String[] args) {
+        /* 
+        Automated Unit Testing --> GitHub Actions 
+        - Modify the pom.xml file 
+        - Create a Workflow called JavaTest Using GitHub Actions
+        - The JDK Version included in the workflow will depend on the 
+          version you used to create the program 
+        */
+    }
+}
